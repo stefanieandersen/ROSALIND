4ddee334505853114a017a3e807e5a40d82cf943
stefanieandersen <effie.andersen@gmail.com>
2017-03-20 11:18:22 -0400

    Update counting_nucleotides.py

--- CountingDNANucleotides/counting_nucleotides.py
+++ CountingDNANucleotides/counting_nucleotides.py
@@ -1,1 +1,25 @@
+##http://rosalind.info/problems/dna/
 
+##input a string of letters
+given_file = open('/home/steffiemarie/Documents/Python/counting.txt')
+
+nucleotides = given_file.read()
+As = 0
+Gs = 0
+Cs = 0
+Ts = 0
+
+##read a letter and if it's the right one then
+for nucleotide in nucleotides:
+    if nucleotide == 'A':
+        As = As + 1
+    elif nucleotide == 'G':
+        Gs = Gs + 1
+    elif nucleotide == 'C':
+        Cs = Cs + 1
+    elif nucleotide == 'T':
+        Ts = Ts + 1
+
+
+print(nucleotides)
+print(As, Gs, Cs, Ts)
