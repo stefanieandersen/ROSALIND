85db3f979e55017f37cfe339e28bfe81ac968037
stefanieandersen <effie.andersen@gmail.com>
2017-03-20 13:44:03 -0400

    Update gc_content.py

--- Computing_GC_Content/gc_content.py
+++ Computing_GC_Content/gc_content.py
@@ -1,36 +1,53 @@
 #http://rosalind.info/problems/gc/
 
-from collections import Counter
-
 rosalind_file = open('rosalind_gc.txt')
 
-best_name = ""
-best_ratio = 0
+strings = rosalind_file.read()
 
-current_name = ""
-current_cg = 0
-current_at = 1
+indexes = []
+index = -1
 
+# Loop while true.
 while True:
-    line = rosalind_file.readline()
-    if line == "" or line[0] == ">":
-        #finish up the previous block
-        current_ratio = (1.0 * current_cg) / (current_cg + current_at)
-        if current_ratio > best_ratio:
-            best_ratio = current_ratio
-            best_name = current_name
-        
-        if (line == ""):
-            break
-        else:
-            #start the new block   
-            current_name = line[1:-1]
-            current_cg = 0
-            current_at = 0
+    # Advance location by 1.
+    index = strings.find(">", index + 1)
+    
+    # Break if not found.
+    if index == -1: 
+        break
     else:
-        linecount = Counter(line)
-        current_at += linecount['A'] + linecount['T']
-        current_cg += linecount['C'] + linecount['G']
+        indexes.append(index)    
 
-print best_name
-print best_ratio
+indexes.append(len(strings))
+
+
+#find the highest cg ratio
+
+iterations = len(indexes) - 1
+AT = {}
+CG = {}
+highest = [0,0]
+
+for i in range(0,iterations):
+    key = strings[indexes[i]+1:indexes[i]+14]
+    AT[key] = 0
+    CG[key] = 0
+
+    for x in strings[indexes[i]:indexes[i+1]]:
+
+        if x == 'A' or x == 'T':
+            AT[key] += 1
+        elif x == 'C' or x == 'G':
+            CG[key] += 1
+        else:
+            pass
+    
+    ratio = (CG[key]*100.0)/(AT[key]+CG[key])
+    if highest[1] >= ratio:
+        pass
+    else:
+        highest[0] = key
+        highest[1] = ratio
+        
+print highest[0]
+print highest[1]
